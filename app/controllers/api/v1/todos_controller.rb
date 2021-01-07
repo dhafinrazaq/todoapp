d176fa6aec6cb56f6fc70e377ac539a21f2771f2
dhafin <dhafinrazaq@gmail.com>
2021-01-07 19:48:41 +0800

    Drop and recreate database

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,2 +1,51 @@
 class Api::V1::TodosController < ApplicationController
+  # GET /todos
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  # GET /todo/:id
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render error: { error: 'Unable to create todo'}, status: 400
+    end
+  end
+
+  # PUT /todos/:id
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo
+      @todo.update(todo_params)
+      render json: { message: 'Todo successfully updated' }, status: 200
+    else
+      render json: { error: 'Unable to update todo'}, status: 400
+    end
+  end
+
+  # DELETE /todos/:id
+  def destroy
+    @todo = Todo.find(params[:id])
+    if @todo
+      @todo.destroy
+      render json: { message: 'Todo successfully deleted' }, status: 200
+    else
+      render json: { error: 'Unable to delete todo'}, status: 400
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :desc)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_133751) do
+ActiveRecord::Schema.define(version: 2021_01_07_114406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "todos", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "name"
+    t.string "desc"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
