25730fe608d6cd74e1100452650e1b93ea7a4f44
dhafin <dhafinrazaq@gmail.com>
2021-01-07 20:34:48 +0800

    Add router to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from "react";
+import { Container } from "reactstrap";
 import TodoList from "./components/Todo/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <TodoList></TodoList>
-    </div>
+    <Router>
+      <Container>
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <React.Fragment>
+              <TodoList></TodoList>
+            </React.Fragment>
+          )}
+        />
+      </Container>
+    </Router>
   );
 }
 
