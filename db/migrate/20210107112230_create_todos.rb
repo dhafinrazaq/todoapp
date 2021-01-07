365080c3f37e5b03c3b4b2312c7eaf83bb4e44d8
dhafin <dhafinrazaq@gmail.com>
2021-01-07 19:30:11 +0800

    Create todo model and todos route

--- db/migrate/20210107112230_create_todos.rb
+++ db/migrate/20210107112230_create_todos.rb
@@ -1,8 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
-      t.string :title
-      t.text :body
+      t.string :name
+      t.string :desc
 
       t.timestamps
     end
---
+++ db/migrate/20210107112407_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :todos do |t|
+      t.string :name
+      t.string :desc
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
