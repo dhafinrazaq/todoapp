4cd9d23754d298bab8ec385e303b42bcb34b7c44
dhafin <dhafinrazaq@gmail.com>
2021-01-12 23:20:46 +0800

    Use hooks and redux on todo list

--- client/src/components/Todo/TodoList.js
+++ client/src/components/Todo/TodoList.js
@@ -1,29 +1,80 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import TodoForm from "./TodoForm";
+import { useSelector, useDispatch } from "react-redux";
+import { ACTIONS } from "../../redux";
 
-export class TodoList extends Component {
-  state = {
-    todos: [],
-  };
-
-  componentDidMount() {
-    axios
-      .get(`/api/v1/todos`)
-      .then((res) => this.setState({ todos: res.data }));
-  }
-
-  render() {
-    return (
-      <div>
-        <TodoForm></TodoForm>
-        {this.state.todos.map((todo) => (
-          <Link to={{ pathname: "/todos/" + todo.id }}>{todo.name}</Link>
-        ))}
-      </div>
-    );
-  }
+async function loadTodos() {
+  const response = await axios.get(`/api/v1/todos`);
+  return response.data;
 }
 
-export default TodoList;
+function useLoadTodos(state) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    loadTodos()
+      .then((data) => {
+        console.log(data);
+        dispatch({
+          type: ACTIONS.LOAD_TODOS,
+          payload: {
+            todos: data,
+          },
+        });
+        console.log("success");
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  }, []);
+
+  return state.todos;
+}
+
+export default function TodoList() {
+  const todos = useSelector(useLoadTodos);
+
+  useEffect(() => {
+    console.log("SERVER_EVENT: todo list changed");
+  }, [todos]);
+
+  return (
+    <div>
+      <TodoForm></TodoForm>
+      <ol>
+        {todos.map((todo) => (
+          <li>
+            <Link to={{ pathname: "/todos/" + todo.id }}>{todo.name}</Link>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+// export class TodoList extends Component {
+//   state = {
+//     todos: [],
+//   };
+
+//   componentDidMount() {
+//     axios
+//       .get(`/api/v1/todos`)
+//       .then((res) => this.setState({ todos: res.data }));
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <TodoForm></TodoForm>
+//         {this.state.todos.map((todo) => (
+//           <Link to={{ pathname: "/todos/" + todo.id }}>{todo.name}</Link>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// export default TodoList;
--- client/src/redux.js
+++ client/src/redux.js
@@ -2,8 +2,8 @@ import { createStore } from "redux";
 
 export const ACTIONS = {
   ADD_TODO: "ADD_TODO",
-  VIEW_TODOS: "VIEW_TODOS",
-  VIEW_TODO: "VIEW_TODO",
+  LOAD_TODOS: "LOAD_TODOS",
+  LOAD_TODO: "LOAD_TODO",
 };
 
 const initialState = {
@@ -13,23 +13,24 @@ const initialState = {
 
 function todoReducer(state = initialState, action) {
   switch (action.type) {
-    case ACTIONS.ADD_TODO: {
+    case ACTIONS.ADD_TODO:
       const { todo } = action.payload;
 
       return {
         ...state,
         todos: state.todos.push(todo),
       };
-    }
 
-    case ACTIONS.VIEW_TODOS: {
+    case ACTIONS.LOAD_TODOS:
       const { todos } = action.payload;
 
       return {
         ...state,
         todos,
       };
-    }
+
+    default:
+      return state;
   }
 }
 
