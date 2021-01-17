fb4df5da7121b3420a5f603c5217d0c8445c6ada
dhafin <dhafinrazaq@gmail.com>
2021-01-17 17:22:29 +0800

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      # todos
       resources :todos do
       end
       get 'todos/tag/:tag', to: 'todos#index_with_tag', as: :tag
 
+      # tags
       resources :tags do
       end
     end
