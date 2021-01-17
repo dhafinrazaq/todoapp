93ee0978defaa2765cf5f91e639bc6e8c30596da
dhafin <dhafinrazaq@gmail.com>
2021-01-17 15:54:17 +0800

    Init migration to typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import React, { Component } from "react";
 import { Container } from "reactstrap";
 import TodoList from "./components/Todo/TodoList";
-import Todo from "./components/Todo/Todo";
 
 function App() {
   return (
